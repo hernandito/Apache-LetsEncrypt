5f530600e7c25ffb458218862b3d8ba72dcc3e92
hernandito <archi@inch.com>
2016-10-11 22:42:50 -0400

    q

--- firstrun.sh
+++ firstrun.sh
@@ -4,14 +4,15 @@
 #export PATH="${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 if [ ! -f /usr/bin/certbot-auto ]; then
-	wget https://dl.eff.org/certbot-auto /usr/bin/
-	chmod a+x /usr/bin/certbot-auto
 	apt-get update
-fi
-
 	certbot-auto --noninteractive --os-packages-only
-	
 	mkdir -p /etc/letsencrypt
 	cp /config/cli.ini /etc/letsencrypt/cli.ini	
 	certbot-auto certonly --noninteractive
+	
+fi
+
+	
+	
+
 

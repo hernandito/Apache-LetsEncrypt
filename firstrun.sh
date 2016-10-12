d6b5b52975241bfa772fc62bfc42726c35dca6a1
hernandito <archi@inch.com>
2016-10-11 22:37:09 -0400

    getting closer

--- firstrun.sh
+++ firstrun.sh
@@ -1,18 +1,17 @@
-crontab /config/crons.conf
+#crontab /config/crons.conf
 # May or may not have HOME set, and this drops stuff into ~/.local.
-export HOME="/root"
-export PATH="${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-
+#export HOME="/root"
+#export PATH="${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 if [ ! -f /usr/bin/certbot-auto ]; then
 	wget https://dl.eff.org/certbot-auto /usr/bin/
 	chmod a+x /usr/bin/certbot-auto
 	apt-get update
+fi
+
 	certbot-auto --noninteractive --os-packages-only
 	
 	mkdir -p /etc/letsencrypt
 	cp /config/cli.ini /etc/letsencrypt/cli.ini	
 	certbot-auto certonly --noninteractive
 
-fi
